c6a579c079130d650fd7196b5413b74202649a7c
squirrelzhang <826903371@qq.com>
2019-09-17 18:14:29 +0800

    swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,35 @@
+<template>
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/5e82991722549bfec5ada28ab25d0820.jpg_750x200_4bb9032d.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/0fa39f9f5e66189e85b5c6e54278587d.jpg_750x200_86c8f2d8.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+   >>> .swiper-pagination-bullet-active
+      background: #ffffff
+    .swiper-img
+      width: 100%
+</style>
