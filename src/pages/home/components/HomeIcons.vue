73ce55576ca37cea0d6c77f258f5fdd1c1bf6850
squirrelzhang <826903371@qq.com>
2019-09-18 10:42:17 +0800

    icon

---
+++ src/pages/home/components/HomeIcons.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="icons">
+    <div class="icon"></div>
+  </div>
+</template>
+<script>
+</script>
+
+<style lang="stylus" scoped>
+  .icons
+    overflow:hidden
+    height:0
+    padding-bottom: 50%
+    background: blue
+    .icon
+      float: left
+      width: 25%
+      padding-bottom: 25%
+      background: red
+</style>
